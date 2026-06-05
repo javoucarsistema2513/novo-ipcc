f06c12f3d31ff39d095f1fe8123350418b78a550
javoucar <javoucarsistema@gmail.com>
2026-06-05 01:07:09 -0300

    subir site

--- vite.config.ts
+++ vite.config.ts
@@ -21,19 +21,19 @@ export default defineConfig(({mode}) => {
           display: 'standalone',
           icons: [
             {
-              src: '/icon-192.png',
+              src: '/icon.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'any'
             },
             {
-              src: '/icon-512.png',
+              src: '/icon.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any'
             },
             {
-              src: '/icon-maskable.png',
+              src: '/icon.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'maskable'
